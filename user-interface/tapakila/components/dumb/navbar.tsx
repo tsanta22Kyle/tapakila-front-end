919b5f2760495133f687e8120cbdb85114b9cad5
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-03-15 11:40:04 +0300

    fix: login request config

--- user-interface/tapakila/components/dumb/navbar.tsx
+++ user-interface/tapakila/components/dumb/navbar.tsx
@@ -2,17 +2,24 @@
 import { useEffect, useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBell, faCaretDown, faCaretUp, faCircleUser, faSearch, faXmark } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBell,
+  faCaretDown,
+  faCaretUp,
+  faCircleUser,
+  faSearch,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
   const [isScroll, setScroll] = useState(false);
-  const [barIsVisible,setVisibleBar] = useState(false);
+  const [barIsVisible, setVisibleBar] = useState(false);
   // const searchbar = document.querySelector(".searchbar")
-  function handleBarVisible(){
-    setVisibleBar(!barIsVisible)
+  function handleBarVisible() {
+    setVisibleBar(!barIsVisible);
     // searchbar?.addEventListener('click',handleBarVisible)
   }
-  console.log(barIsVisible)
+  console.log(barIsVisible);
   useEffect(() => {
     function handleScroll() {
       if (window.scrollY > 50) {
@@ -30,42 +37,54 @@ function Navbar() {
         <li className="logo nav-element">
           <h1>TAPAKILA</h1>
         </li>
-        <li className="nav-element">
-          <a href="#" className="element-link">
-            catégories
+        <li className="nav-element dropdown-container">
+          <a href="" className="element-link">
+
+          <p>catégories</p>
           </a>
-    
+        </li>
+        <li className="nav-element search-nav">
+          <form
+            action=""
+            className={`searchbar ${barIsVisible ? "full-width" : ""}  ${
+              isScroll ? "dark-searchbar" : "light-searchbar"
+            }`}
+          >
+            <input
+              type="text"
+              placeholder="évènement , organisateur , lieu..."
+              id="searchbar"
+              className={`${barIsVisible ? "show-input" : "collapse-input"}  ${
+                isScroll ? "dark-searchbar" : ""
+              }`}
+            />
+            <div onClick={handleBarVisible} className="search-icon">
+              <FontAwesomeIcon
+                icon={faSearch}
+                className={`fas fa-search fa-xl search ${
+                  barIsVisible ? "collapse" : ""
+                }`}
+              ></FontAwesomeIcon>
+              <FontAwesomeIcon
+                icon={faXmark}
+                className={` fas fa-xl close-search ${
+                  barIsVisible ? "" : "collapse"
+                }`}
+              />
+            </div>
+          </form>
         </li>
         <li className="nav-element">
           <a href="#" className="element-link">
             lieux
           </a>
         </li>
-        <li className="nav-element">
-          <form action="" className={`searchbar ${ barIsVisible ? "full-width" : "" }  ${isScroll ? "dark-searchbar":"light-searchbar"}`}>
-            <input
-              type="text"
-              placeholder="évènement , organisateur , lieu..."
-              id="searchbar"
-              className={`${barIsVisible ? "show-input":"collapse-input"}  ${isScroll ? "dark-searchbar":""}`}
-            />
-            <div onClick={handleBarVisible} className="search-icon" >
-              <FontAwesomeIcon
-                icon={faSearch}
-                className={`fas fa-search fa-xl search ${barIsVisible ? "collapse":""}`}
-              ></FontAwesomeIcon>
-              <FontAwesomeIcon icon={faXmark} className={` fas fa-xl close-search ${barIsVisible ? "":"collapse"}`} />
-            </div>
-          </form>
-        </li>
-        <li className="nav-element">
+        <li className="nav-element user-nav">
           <a href="" className="element-link">
-          <FontAwesomeIcon icon={faBell} className="fas fa-2xl" />
+            <FontAwesomeIcon icon={faBell} className="fas fa-2xl" />
           </a>
-        </li>
-        <li className="nav-element">
           <a href="" className="element-link">
-          <FontAwesomeIcon icon={faCircleUser} className="fa-2xl fas"/>
+            <FontAwesomeIcon icon={faCircleUser} className="fa-2xl fas" />
           </a>
         </li>
       </ul>
