c3b0247dcedb85ab6655a105aced57a9520761bc
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-06-18 20:26:45 +0300

    fix:parser

--- user-interface/tapakila/postcss.config.js
+++ user-interface/tapakila/postcss.config.js
@@ -1,9 +1,10 @@
 const safeParser = require("postcss-safe-parser");
 
 module.exports = {
-  parser: safeParser,
+  parser: safeParser, // évite les erreurs à la con sur les slashs, etc.
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    // PAS DE cssnano ici !
   },
 };
